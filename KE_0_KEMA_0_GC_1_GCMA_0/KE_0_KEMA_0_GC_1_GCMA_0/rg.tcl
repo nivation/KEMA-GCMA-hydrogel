13584b618c63098b9ac5898e43dfd7a9de01e811
YC Lai <67781344+nivation@users.noreply.github.com>
2022-07-19 18:20:43 +0800

    Delete KE_0_KEMA_0_GC_1_GCMA_0 directory

--- KE_0_KEMA_0_GC_1_GCMA_0/KE_0_KEMA_0_GC_1_GCMA_0/rg.tcl
+++
@@ -1,8 +0,0 @@
-set total_frame [molinfo top get frame]
-set a [atomselect top "fragment 0"]
-set gc {}
-for {set i 0} {$i < $total_frame} {incr i} {
-	animate goto $i
-	lappend gc [measure rgyr $a]
-}
-puts $gc
